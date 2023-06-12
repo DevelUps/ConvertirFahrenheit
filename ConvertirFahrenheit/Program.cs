2002ce29e2d81ab699c461748e12787115afdb1c
Pierre <Pierre@DESKTOP-UJADA9E>
2023-06-11 20:38:52 -0500

    Agregar archivos de proyecto.

---
+++ ConvertirFahrenheit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertirFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            float gradosC, gradosF;
+            // pedimos al usuario ingresar los valores
+            Console.Write("Por favor ingresa los grados °C:   ");
+            // se inicializa la variable y se convierte el string recibido
+            gradosC = float.Parse(Console.ReadLine());
+            // Realizamos la convercion de °C a °F
+            gradosF = (gradosC * 1.8f) + 32;
+            // Mostramos resultado en pantala
+            Console.WriteLine("{0}C° Convertidos en Fahrenheit es {1} °F, ", gradosC, gradosF);
+        }
+    }
+}
